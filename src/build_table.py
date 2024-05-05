52351cb9cd7eab3a78de735d53b71f1ee61186ef
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-05-05 13:40:11 -0400

    run from parent dir

--- src/build_table.py
+++ src/build_table.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 BASE_URL = "https://leetcode-stats-api.herokuapp.com/"
-USERLIST = "../bin/users.json"
+USERLIST = "bin/users.json"
 
 with open(USERLIST, 'r') as u:
     global users
@@ -10,7 +10,7 @@ with open(USERLIST, 'r') as u:
 
 rankings = []
 
-for user, username in users.items():
+for username, user in users.items():
     res = requests.get(BASE_URL + username).json()
     cal = res['submissionCalendar']
     submits = sum(cal.values())
@@ -20,7 +20,7 @@ rankings.sort()
 rankings.reverse()
 
 # make table
-with open("../readme.md", 'w') as f:
+with open("readme.md", 'w') as f:
     f.write("| Name | Submissions |\n")
     for submits, user in rankings:
         f.write(f"| {user} | {submits} |\n")
