9668d0878014582e023501c64808906cf703b6f7
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-05-05 12:57:22 -0400

    add user by command line:

---
+++ src/add_users.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import sys
+
+BASE_URL = "https://leetcode-stats-api.herokuapp.com/"
+USERLIST = "../bin/users.json"
+
+with open(USERLIST, 'r') as u:
+    global users
+    users = json.load(u)
+
+if len(sys.argv) != 4:
+    exit(0)
+
+username = sys.argv[1]
+firstname = sys.argv[2]
+lastname = sys.argv[3]
+
+if username in users.keys():
+    exit(0)
+
+res = requests.get(BASE_URL + username).json()
+
+if res['status'] != 'success':
+    exit(0)
+
+users[username] = f"{firstname} {lastname}"
+
+with open(USERLIST) as output:
+    output.write(json.dumps(users))
