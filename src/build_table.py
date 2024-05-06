67a6b2dfa7230443559865a72d6c72212e616171
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-05-06 12:28:32 -0400

    code block

--- src/build_table.py
+++ src/build_table.py
@@ -23,7 +23,7 @@ with open("readme.md", 'w') as f:
     f.write("# Leeterboard\n")
     f.write("an automatic leaderboard for leetcode stats\n")
     f.write("## How to join\n")
-    f.write('Open a new issue with the title : <leetcode username> <first name> <last name>  \n')
+    f.write('Open a new issue with the title : `<leetcode username> <first name> <last name>`  \n')
     f.write('For example, I would open a new issue titled : bobbypalmer Bobby Palmer  \n')
     f.write("## Scoring\n")
     f.write("You score is calculated as : 3 * hards solved + 2 * mediums solved + 1 * easys solved  \n")
