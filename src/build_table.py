a529720b1c87aeafacfa9fcd498f23a99b86ffa9
Conner Rose <conner.n.rose@gmail.com>
2024-05-06 13:25:14 -0400

    Add difficulty columns

--- src/build_table.py
+++ src/build_table.py
@@ -14,7 +14,15 @@ rankings = []
 for username, user in users.items():
     res = requests.get(BASE_URL + username).json()
     score = res["easySolved"] + 2 * res["mediumSolved"] + 3 * res["hardSolved"]
-    rankings.append((score, user))
+    rankings.append(
+        (
+            score,
+            user,
+            res["easySolved"],
+            res["mediumSolved"],
+            res["hardSolved"],
+        )
+    )
 
 rankings.sort()
 rankings.reverse()
@@ -28,7 +36,7 @@ with open("readme.md", "w") as f:
         f.write(line)
         if line.strip() == "## Leaderboard":
             break
-    f.write("| Name | Score |\n")
-    f.write("| --- | --- |\n")
-    for score, user in rankings:
-        f.write(f"| {user} | {score} |\n")
+    f.write("| Name | Easy | Medium | Hard | Score |\n")
+    f.write("| --- | --- | --- | --- | --- |\n")
+    for score, user, easy, medium, hard in rankings:
+        f.write(f"| {user} | {easy} | {medium} | {hard} | {score} |\n")
