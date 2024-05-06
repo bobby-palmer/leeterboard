9e8c56a59e4fc0a41988ea4972c2053d984d3dba
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-05-06 12:09:14 -0400

    score on accpeted not submissions

--- src/build_table.py
+++ src/build_table.py
@@ -12,17 +12,16 @@ rankings = []
 
 for username, user in users.items():
     res = requests.get(BASE_URL + username).json()
-    cal = res['submissionCalendar']
-    submits = sum(cal.values())
-    rankings.append((submits, user))
+    score = res["easySolved"] + 2 * res["mediumSolved"] + 3 * res["hardSolved"]
+    rankings.append((score, user))
 
 rankings.sort()
 rankings.reverse()
 
 # make table
 with open("readme.md", 'w') as f:
-    f.write("| Name | Submissions |\n")
+    f.write("| Name | Score |\n")
     f.write("| --- | --- |\n")
-    for submits, user in rankings:
-        f.write(f"| {user} | {submits} |\n")
+    for score, user in rankings:
+        f.write(f"| {user} | {score} |\n")
 
